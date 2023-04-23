cc30b0a5d0925831fb4582432cd451ad1bed899f
Luis Rafael Segura <lrseguramartinez@gmail.com>
2023-04-23 10:00:31 -0400

    Adding logout

--- src/main/java/com/code2ever/shoppinglist/security/config/SecurityConfig.java
+++ src/main/java/com/code2ever/shoppinglist/security/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.code2ever.shoppinglist.security.config;
 
 import com.code2ever.shoppinglist.security.CsrfTokenLogger;
+import jakarta.servlet.ServletException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +13,7 @@ import org.springframework.security.web.csrf.CsrfFilter;
 
 
 @Configuration
+@Slf4j
 public class SecurityConfig {
 
 //    @Bean
@@ -26,11 +29,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http.httpBasic();
         http.addFilterAfter(new CsrfTokenLogger(), CsrfFilter.class);
 //        http.addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class);
         http.authorizeHttpRequests((authorize) -> authorize.requestMatchers("/api/auth/**").hasRole("SECURITY"))
                 .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());
+        http.logout(logout -> logout.logoutUrl("/api/auth/logout").addLogoutHandler(((request, response, authentication) -> {
+            try {
+                request.logout();
+            } catch (ServletException e) {
+                log.error(e.getMessage());
+            }
+        })));
         return http.build();
     }
 }
